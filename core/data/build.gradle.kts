b35bca1db4efe9924b6e34b45dc1cf950f7a8500
largeblueberry <onlym3480@naver.com>
2025-11-26 20:17:31 +0900

    [feat] : core:data 모듈 추가 - history 기능 개발 준비

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -2,12 +2,10 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     id("com.google.devtools.ksp")
-    alias(libs.plugins.compose.compiler)
-    id("com.google.dagger.hilt.android")
 }
 
 android {
-    namespace = "com.largeblueberry.dynamicdetail"
+    namespace = "com.largeblueberry.data"
     compileSdk = 36
 
     defaultConfig {
@@ -36,7 +34,6 @@ android {
 }
 
 dependencies {
-    implementation(project(":core:data"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
@@ -45,33 +42,10 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    ksp("com.google.dagger:hilt-android-compiler:2.56.2")
 
-    // Navigation
-    implementation("androidx.navigation:navigation-compose:2.8.4")
-
-    // ===== COMPOSE =====
-    // Compose BOM - 모든 Compose 라이브러리 버전 관리
-    implementation(platform("androidx.compose:compose-bom:2025.05.00"))
-
-    androidTestImplementation(platform("androidx.compose:compose-bom:2025.05.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.foundation:foundation")
-    // Material Design 3 (권장)
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-extended")
-    // Compose 통합
-    implementation("androidx.activity:activity-compose:1.10.1")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
     val roomVersion = "2.6.1"
     implementation("androidx.room:room-runtime:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
     ksp("androidx.room:room-compiler:$roomVersion") // ksp 플러그인 필요
-
-    // Hilt
-    implementation("com.google.dagger:hilt-android:2.56.2")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 }
